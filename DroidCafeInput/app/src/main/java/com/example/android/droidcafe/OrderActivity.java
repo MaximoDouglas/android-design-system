7f0b6f1eb46c5af0caba56fb786b630d15bbd0cb
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-05-30 15:20:15 -0300

    codelab 4.2 - done

--- DroidCafeInput/app/src/main/java/com/example/android/droidcafe/OrderActivity.java
+++ DroidCafeInput/app/src/main/java/com/example/android/droidcafe/OrderActivity.java
@@ -21,7 +21,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +37,21 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
 
         // Get the intent and its data.
         Intent intent = getIntent();
@@ -72,5 +89,16 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
