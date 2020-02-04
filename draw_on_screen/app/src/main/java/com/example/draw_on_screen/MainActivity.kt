c9e1a0b0c9e6d608831bf86186bcef6bbe921369
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-02-04 16:01:31 -0300

    origin/master Initial commit
    -
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

---
+++ draw_on_screen/app/src/main/java/com/example/draw_on_screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.draw_on_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
