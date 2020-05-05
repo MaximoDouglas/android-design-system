a42f78d3974c72e5e1fabaafbcb233e68f2f574b
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-04 23:19:26 -0300

    Add new design systems project

---
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/modules/selectDesignSystem/SelectDesignSystemFragment.kt
@@ -0,0 +1,45 @@
+package br.com.argmax.design_systems.app.modules.selectDesignSystem
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import br.com.argmax.design_systems.R
+import br.com.argmax.design_systems.databinding.SelectDesignSystemFragmentBinding
+
+class SelectDesignSystemFragment : Fragment() {
+
+    private var mBinding: SelectDesignSystemFragmentBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.select_design_system_fragment,
+            container,
+            false
+        )
+
+        return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        mBinding?.selectDesignSystemAsphaltButton?.setOnClickListener {
+            Toast.makeText(context, "Asphalt", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
