162e5b81a1624e6e14c7dfbf4f07a86c09e4b898
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-11 02:44:47 -0300

    Add social avatar component implementation class

--- argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltSocialAvatarBadgeComponentView.kt
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltSocialAvatarBadgeComponentView.kt
@@ -2,53 +2,53 @@ package br.com.argmax.design_systems.app.components.asphalt
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import br.com.argmax.design_systems.R
-import br.com.argmax.design_systems.databinding.AsphaltNotificationBadgeComponentViewBinding
+import br.com.argmax.design_systems.app.setCircularImageByUrl
+import br.com.argmax.design_systems.databinding.AsphaltSocialAvatarBadgeComponentViewBinding
 
 class AsphaltSocialAvatarBadgeComponentView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
 ) : LinearLayout(context, attrs, defStyle) {
 
-    private var mBinding: AsphaltNotificationBadgeComponentViewBinding? =
+    private var mBinding: AsphaltSocialAvatarBadgeComponentViewBinding? =
         DataBindingUtil.inflate(
             LayoutInflater.from(context),
-            R.layout.asphalt_notification_badge_component_view,
+            R.layout.asphalt_social_avatar_badge_component_view,
             this,
             true
         )
 
-    fun setQuantity(quantity: Int): Boolean {
-        return if (isValidQuantity(quantity)) {
-            updateView(quantity)
-            true
-        } else {
+    fun setImageUrls(imageUrls: List<String>, badgeLabel: String): Boolean {
+        return if (imageUrls.isEmpty()) {
             false
+        } else {
+            setupImageViewResources(imageUrls)
+            setupLabelText(badgeLabel)
+
+            mBinding?.executePendingBindings()
+            return true
         }
     }
 
-    private fun isValidQuantity(quantity: Int): Boolean {
-        return quantity in 0..99
+    private fun setupImageViewResources(imageUrls: List<String>) {
+        val mainImageUrl = imageUrls[0]
+
+        mBinding?.asphaltSocialAvatarBadgeMainImage?.setCircularImageByUrl(mainImageUrl)
+
+        if (imageUrls.size > 1) {
+            val secondaryImageUrl = imageUrls[1]
+
+            mBinding?.asphaltSocialAvatarBadgeSecondaryImage?.setCircularImageByUrl(
+                secondaryImageUrl
+            )
+        }
     }
 
-    private fun updateView(quantity: Int) {
-        val params = mBinding?.asphaltDesignSystemNotificationBadgeTextView?.layoutParams
-        val width  = 16
-        val height = if (quantity in 0..9) 16 else 14
+    private fun setupLabelText(badgeLabel: String) {
 
-        params?.width  = convertPxToDp(width)
-        params?.height = convertPxToDp(height)
-
-        mBinding?.asphaltDesignSystemNotificationBadgeTextView?.text         = quantity.toString()
-        mBinding?.asphaltDesignSystemNotificationBadgeTextView?.layoutParams = params
-        mBinding?.executePendingBindings()
-    }
-
-    private fun convertPxToDp(pixelsSize: Int): Int {
-        return (pixelsSize * context.resources.displayMetrics.densityDpi) / DisplayMetrics.DENSITY_DEFAULT
     }
 
 
