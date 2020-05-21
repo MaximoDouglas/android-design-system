968d2eb0072b998afd3bd6d7831a623e63474bb5
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-20 22:07:03 -0300

    Delete override of onTouchListener for nude button

--- argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltNudeButtonComponentView.kt
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltNudeButtonComponentView.kt
@@ -1,12 +1,9 @@
 package br.com.argmax.design_systems.app.components.asphalt
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import br.com.argmax.design_systems.R
@@ -20,10 +17,6 @@ class AsphaltNudeButtonComponentView @JvmOverloads constructor(
         DataBindingUtil.inflate(
             LayoutInflater.from(context), R.layout.asphalt_nude_button_component_view, this, true
         )
-
-    init {
-        onPressedListener()
-    }
 
     override fun setEnabled(enabled: Boolean) {
         super.setEnabled(enabled)
@@ -40,29 +33,6 @@ class AsphaltNudeButtonComponentView @JvmOverloads constructor(
     fun setText(text: String) {
         mAsphaltNudeButtonComponentViewBinding?.asphaltNudeButtonLabelTextView?.text = text
         mAsphaltNudeButtonComponentViewBinding?.executePendingBindings()
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private fun onPressedListener() {
-        mAsphaltNudeButtonComponentViewBinding?.asphaltNudeButtonLabelTextView?.setOnTouchListener { textView, event ->
-            if (this.isEnabled) {
-                if (event.action == MotionEvent.ACTION_DOWN) {
-                    (textView as TextView).setTextColor(
-                        ContextCompat.getColor(context, R.color.RED40)
-                    )
-                }
-
-                if (event.action == MotionEvent.ACTION_BUTTON_RELEASE) {
-                    (textView as TextView).setTextColor(
-                        ContextCompat.getColor(context, R.color.RED60)
-                    )
-                }
-
-                textView.performClick()
-            } else {
-                true
-            }
-        }
     }
 
 
