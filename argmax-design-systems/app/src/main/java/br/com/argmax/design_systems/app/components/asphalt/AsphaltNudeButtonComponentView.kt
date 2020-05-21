eb1228f31b7e3e8b7b9bcf6d16cf0deb68114fea
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-05-20 21:12:27 -0300

    Set on pressed nude button layout change

--- argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltNudeButtonComponentView.kt
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltNudeButtonComponentView.kt
@@ -1,11 +1,15 @@
 package br.com.argmax.design_systems.app.components.asphalt
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import br.com.argmax.design_systems.R
 import br.com.argmax.design_systems.databinding.AsphaltNudeButtonComponentViewBinding
@@ -19,9 +23,27 @@ class AsphaltNudeButtonComponentView @JvmOverloads constructor(
             LayoutInflater.from(context), R.layout.asphalt_nude_button_component_view, this, true
         )
 
+    init {
+        onPressedListener()
+    }
+
     fun setText(text: String) {
         mAsphaltNudeButtonComponentViewBinding?.asphaltNudeButtonLabelTextView?.text = text
         mAsphaltNudeButtonComponentViewBinding?.executePendingBindings()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun onPressedListener() {
+        mAsphaltNudeButtonComponentViewBinding?.asphaltNudeButtonLabelTextView?.setOnTouchListener { textView, event ->
+            (textView as TextView).setTextColor(
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    ContextCompat.getColor(context, R.color.RED40)
+                } else {
+                    ContextCompat.getColor(context, R.color.RED60)
+                }
+            )
+            textView.performClick()
+        }
     }
 
 
