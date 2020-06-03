a1ee4f56a31618ff8fd09bfdef39784525366b89
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-02 21:34:44 -0300

    Refactor on Asphalt alert badge component view

--- argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/MainActivity.kt
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/MainActivity.kt
@@ -10,4 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
--- argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltAlertBadgeComponentView.kt
+++ argmax-design-systems/app/src/main/java/br/com/argmax/design_systems/app/components/asphalt/AsphaltAlertBadgeComponentView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import br.com.argmax.design_systems.R
 import br.com.argmax.design_systems.databinding.AsphaltAlertBadgeComponentViewBinding
@@ -13,19 +12,14 @@ class AsphaltAlertBadgeComponentView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
 ) : LinearLayout(context, attrs, defStyle) {
 
-    private var mAsphaltAlertBadgeComponentViewBinding: AsphaltAlertBadgeComponentViewBinding? =
+    private var mBinding: AsphaltAlertBadgeComponentViewBinding? =
         DataBindingUtil.inflate(
             LayoutInflater.from(context), R.layout.asphalt_alert_badge_component_view, this, true
         )
 
-    init {
-        setupNudeButton()
+    fun setText(text: String) {
+        mBinding?.asphaltDesignSystemFragmentAlertBadgeText?.text = text
     }
 
-    private fun setupNudeButton() {
-        mAsphaltAlertBadgeComponentViewBinding?.asphaltDesignSystemFragmentAlertBadgeNudeButton?.setOnClickListener {
-            Toast.makeText(context, "Alert Badge Nude Button Clicked", Toast.LENGTH_LONG).show()
-        }
-    }
 
 }
