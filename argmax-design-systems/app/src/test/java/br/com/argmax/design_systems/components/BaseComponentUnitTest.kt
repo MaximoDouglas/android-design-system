0b0868056833743bd4cc6a5b00f9a2eb98087a1f
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-03 22:37:02 -0300

    Add Base Component Unit Test
    - Add abstract class for the component unit tests
    - Add robolectric as a test dependency
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

---
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/BaseComponentUnitTest.kt
@@ -0,0 +1,39 @@
+package br.com.argmax.design_systems.components
+
+import android.app.Activity
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
+
+@RunWith(RobolectricTestRunner::class)
+abstract class BaseComponentUnitTest {
+
+    private lateinit var activityController: ActivityController<Activity>
+
+    @Before
+    fun setup() {
+        activityController = Robolectric.buildActivity(Activity::class.java)
+            .create()
+            .start()
+            .resume()
+            .visible()
+
+        val activity: Activity = activityController.get()
+        setupComponent(activity)
+    }
+
+    abstract fun setupComponent(activity: Activity)
+
+    @After
+    fun tearDown() {
+        activityController
+            .pause()
+            .stop()
+            .destroy()
+    }
+
+
+}
