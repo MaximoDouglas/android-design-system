8f7df78031d0eaeaaeb7df5136702c7dc943160a
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-10 02:11:59 -0300

    Complete Asphal Alert Badge component tests

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/AsphaltAlertBadgeSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/AsphaltAlertBadgeSuiteTest.kt
@@ -1,6 +1,7 @@
 package br.com.argmax.design_systems.components.asphalt
 
 import br.com.argmax.design_systems.components.asphalt.context.AsphaltAlertBadgeWithClickListenerTest
+import br.com.argmax.design_systems.components.asphalt.context.AsphaltAlertBadgeWithTextAndClickListenerTest
 import br.com.argmax.design_systems.components.asphalt.context.AsphaltAlertBadgeWithTextTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -8,6 +9,7 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     AsphaltAlertBadgeWithTextTest::class,
-    AsphaltAlertBadgeWithClickListenerTest::class
+    AsphaltAlertBadgeWithClickListenerTest::class,
+    AsphaltAlertBadgeWithTextAndClickListenerTest::class
 )
 class AsphaltAlertBadgeSuiteTest
---
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/context/AsphaltAlertBadgeWithTextAndClickListenerTest.kt
@@ -0,0 +1,22 @@
+package br.com.argmax.design_systems.components.asphalt.context
+
+import org.junit.Test
+
+class AsphaltAlertBadgeWithTextAndClickListenerTest : AsphaltAlertBadgeTest() {
+
+    @Test
+    fun `test if text is set correctly`() {
+        `when alert badge has text`()
+        `when alert badge has click listener`()
+        `assert alert badge text is set correctly`()
+    }
+
+    @Test
+    fun `test if there is click listener`() {
+        `when alert badge has text`()
+        `when alert badge has click listener`()
+        `assert alert badge has click listeners`()
+    }
+
+
+}
