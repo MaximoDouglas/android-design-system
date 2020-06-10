f6ba6662fb9ced599141ca1463a506e468c3dccf
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-10 01:46:12 -0300

    Update component tests suite

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/AsphaltAlertBadgeSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/AsphaltAlertBadgeSuiteTest.kt
@@ -1,11 +1,13 @@
 package br.com.argmax.design_systems.components.asphalt
 
+import br.com.argmax.design_systems.components.asphalt.context.AsphaltAlertBadgeWithClickListenerTest
 import br.com.argmax.design_systems.components.asphalt.context.AsphaltAlertBadgeWithTextTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    AsphaltAlertBadgeWithTextTest::class
+    AsphaltAlertBadgeWithTextTest::class,
+    AsphaltAlertBadgeWithClickListenerTest::class
 )
 class AsphaltAlertBadgeSuiteTest
