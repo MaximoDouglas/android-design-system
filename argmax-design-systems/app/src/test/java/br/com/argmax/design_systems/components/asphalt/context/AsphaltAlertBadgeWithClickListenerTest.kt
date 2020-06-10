2d41835709b90e891d15f980b1826fdd3acfe3f4
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-10 01:40:09 -0300

    Add Alert Badge Component test context when it has click listeners

---
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/context/AsphaltAlertBadgeWithClickListenerTest.kt
@@ -0,0 +1,20 @@
+package br.com.argmax.design_systems.components.asphalt.context
+
+import org.junit.Test
+
+class AsphaltAlertBadgeWithClickListenerTest : AsphaltAlertBadgeTest() {
+
+    @Test
+    fun `test if there is click listener`() {
+        `when alert badge has click listener`()
+        `assert alert badge has click listeners`()
+    }
+
+    @Test
+    fun `test if there no text`() {
+        `when alert badge has click listener`()
+        `assert alert badge text is null or empty`()
+    }
+
+
+}
