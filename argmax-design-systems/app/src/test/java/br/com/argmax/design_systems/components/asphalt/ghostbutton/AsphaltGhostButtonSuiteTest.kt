ca7fc086a174d200ba56129f7f63f9b79f25af14
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-11 22:57:15 -0300

    Add ghost button conexts and assertions

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/AsphaltGhostButtonSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/AsphaltGhostButtonSuiteTest.kt
@@ -1,15 +1,15 @@
 package br.com.argmax.design_systems.components.asphalt.ghostbutton
 
-import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonEnabledTest
-import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonEnabledWithTextTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonDisabledTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonDisabledWithTextTest
 import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonWithTextTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    AsphaltGhostButtonEnabledTest::class,
+    AsphaltGhostButtonDisabledTest::class,
     AsphaltGhostButtonWithTextTest::class,
-    AsphaltGhostButtonEnabledWithTextTest::class
+    AsphaltGhostButtonDisabledWithTextTest::class
 )
 class AsphaltGhostButtonSuiteTest
--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/context/AsphaltGhostButtonDisabledTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/context/AsphaltGhostButtonDisabledTest.kt
@@ -1,5 +1,5 @@
 package br.com.argmax.design_systems.components.asphalt.ghostbutton.context
 
-class AsphaltGhostButtonEnabledTest {
+class AsphaltGhostButtonDisabledTest {
 
 }
