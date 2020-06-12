514f7cbc38e87aa0df8c23719171e6e24ffea995
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-11 22:30:44 -0300

    Add new component tests into test suites

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/ComponentSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/ComponentSuiteTest.kt
@@ -1,11 +1,13 @@
 package br.com.argmax.design_systems.components
 
 import br.com.argmax.design_systems.components.asphalt.alertbadge.AsphaltAlertBadgeSuiteTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.AsphaltGhostButtonSuiteTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    AsphaltAlertBadgeSuiteTest::class
+    AsphaltAlertBadgeSuiteTest::class,
+    AsphaltGhostButtonSuiteTest::class
 )
 class ComponentSuiteTest
--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/AsphaltGhostButtonSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/ghostbutton/AsphaltGhostButtonSuiteTest.kt
@@ -1,15 +1,15 @@
 package br.com.argmax.design_systems.components.asphalt.ghostbutton
 
-import br.com.argmax.design_systems.components.asphalt.alertbadge.context.AsphaltAlertBadgeWithClickListenerTest
-import br.com.argmax.design_systems.components.asphalt.alertbadge.context.AsphaltAlertBadgeWithTextAndClickListenerTest
-import br.com.argmax.design_systems.components.asphalt.alertbadge.context.AsphaltAlertBadgeWithTextTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonEnabledTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonEnabledWithTextTest
+import br.com.argmax.design_systems.components.asphalt.ghostbutton.context.AsphaltGhostButtonWithTextTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    AsphaltAlertBadgeWithTextTest::class,
-    AsphaltAlertBadgeWithClickListenerTest::class,
-    AsphaltAlertBadgeWithTextAndClickListenerTest::class
+    AsphaltGhostButtonEnabledTest::class,
+    AsphaltGhostButtonWithTextTest::class,
+    AsphaltGhostButtonEnabledWithTextTest::class
 )
 class AsphaltGhostButtonSuiteTest
