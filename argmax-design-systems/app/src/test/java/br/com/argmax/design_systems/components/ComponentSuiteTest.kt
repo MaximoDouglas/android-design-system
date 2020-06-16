476cecf1bbc7fda507d2bb55258500470737b467
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-15 22:41:38 -0300

    Add Asphalt Notification Badge Component Test Structure

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/ComponentSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/ComponentSuiteTest.kt
@@ -2,12 +2,14 @@ package br.com.argmax.design_systems.components
 
 import br.com.argmax.design_systems.components.asphalt.alertbadge.AsphaltAlertBadgeSuiteTest
 import br.com.argmax.design_systems.components.asphalt.ghostbutton.AsphaltGhostButtonSuiteTest
+import br.com.argmax.design_systems.components.asphalt.notificationbadge.AsphaltNotificationBadgeSuiteTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     AsphaltAlertBadgeSuiteTest::class,
-    AsphaltGhostButtonSuiteTest::class
+    AsphaltGhostButtonSuiteTest::class,
+    AsphaltNotificationBadgeSuiteTest::class
 )
 class ComponentSuiteTest
--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/notificationbadge/AsphaltNotificationBadgeSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/notificationbadge/AsphaltNotificationBadgeSuiteTest.kt
@@ -1,5 +1,14 @@
 package br.com.argmax.design_systems.components.asphalt.notificationbadge
 
+import br.com.argmax.design_systems.components.asphalt.notificationbadge.context.AsphaltNotificationBadgeTest
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.junit.runners.Suite
+
+@RunWith(JUnit4::class)
+@Suite.SuiteClasses(
+    AsphaltNotificationBadgeTest::class
+)
 class AsphaltNotificationBadgeSuiteTest {
 
 }
