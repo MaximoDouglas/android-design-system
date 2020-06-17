c43fe894efea3454a74b4569eb8994cfde04b178
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-16 22:28:27 -0300

    Finish Asphalt Notification Badge component test

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/notificationbadge/AsphaltNotificationBadgeSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/notificationbadge/AsphaltNotificationBadgeSuiteTest.kt
@@ -1,6 +1,7 @@
 package br.com.argmax.design_systems.components.asphalt.notificationbadge
 
 import br.com.argmax.design_systems.components.asphalt.notificationbadge.context.AsphaltNotificationBadgeInvalidQuantityTest
+import br.com.argmax.design_systems.components.asphalt.notificationbadge.context.AsphaltNotificationBadgeNoQuantityTest
 import br.com.argmax.design_systems.components.asphalt.notificationbadge.context.AsphaltNotificationBadgeValidQuantityTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -8,6 +9,7 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
     AsphaltNotificationBadgeValidQuantityTest::class,
-    AsphaltNotificationBadgeInvalidQuantityTest::class
+    AsphaltNotificationBadgeInvalidQuantityTest::class,
+    AsphaltNotificationBadgeNoQuantityTest::class
 )
 class AsphaltNotificationBadgeSuiteTest
