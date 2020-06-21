ee627308d32d81ff4fb7c60526e16a9f75654af4
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-20 23:55:42 -0300

    Add setup scenario to Asphalt Social Badge component test with one image and label

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeTest.kt
@@ -6,10 +6,22 @@ import br.com.argmax.design_systems.components.asphalt.AsphaltSocialAvatarBadge
 
 abstract class AsphaltSocialAvatarBadgeTest : BaseComponentUnitTest() {
 
+    companion object {
+
+        private val IMAGE_URL_LIST_WITH_ONE_VALID_URL = listOf("https://picsum.photos/200/300")
+
+        private const val TEXT_LABEL = "Lorem Ipsum é simplesmente uma simulação"
+
+    }
+
     private var mAsphaltSocialAvatarBadge: AsphaltSocialAvatarBadge? = null
 
     override fun setupComponent(activity: Activity) {
         mAsphaltSocialAvatarBadge = AsphaltSocialAvatarBadge(activity)
+    }
+
+    fun `when social avatar badge has one valid url and label test`() {
+        mAsphaltSocialAvatarBadge?.setLabelAndImageUrlList(IMAGE_URL_LIST_WITH_ONE_VALID_URL, TEXT_LABEL)
     }
 
 }
--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeWithOneValidUrlAndLabelTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeWithOneValidUrlAndLabelTest.kt
@@ -1,5 +1,12 @@
 package br.com.argmax.design_systems.components.asphalt.socialavatarbadge.context
 
-class AsphaltSocialAvatarBadgeWithOneValidUrlAndLabelTest {
+import org.junit.Before
+
+class AsphaltSocialAvatarBadgeWithOneValidUrlAndLabelTest : AsphaltSocialAvatarBadgeTest() {
+
+    @Before
+    fun `setup scenario`() {
+        `when social avatar badge has one valid url and label test`()
+    }
 
 }
