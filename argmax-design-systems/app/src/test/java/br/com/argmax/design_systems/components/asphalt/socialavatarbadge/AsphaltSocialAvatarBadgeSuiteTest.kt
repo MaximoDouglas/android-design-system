7ba54a525da70db54677847b8e971f879a71391c
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-06-29 19:12:12 -0300

    Refactor on social avatar component test class name

--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/AsphaltSocialAvatarBadgeSuiteTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/AsphaltSocialAvatarBadgeSuiteTest.kt
@@ -6,7 +6,7 @@ import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    AsphaltSocialAvatarBadgeWithLabelAndInvalidUrlTest::class,
+    AsphaltSocialAvatarBadgeWithInvalidUrlAndLabelTest::class,
     AsphaltSocialAvatarBadgeWithOneValidUrlAndLabelTest::class,
     AsphaltSocialAvatarBadgeWithTwoValidUrlAndLabelTest::class
 )
--- argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeWithInvalidUrlAndLabelTest.kt
+++ argmax-design-systems/app/src/test/java/br/com/argmax/design_systems/components/asphalt/socialavatarbadge/context/AsphaltSocialAvatarBadgeWithInvalidUrlAndLabelTest.kt
@@ -1,5 +1,5 @@
 package br.com.argmax.design_systems.components.asphalt.socialavatarbadge.context
 
-class AsphaltSocialAvatarBadgeWithLabelAndInvalidUrlTest {
+class AsphaltSocialAvatarBadgeWithInvalidUrlAndLabelTest {
 
 }
