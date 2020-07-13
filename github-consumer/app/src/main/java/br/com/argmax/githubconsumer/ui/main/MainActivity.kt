81b18a4a6e02b3773fe2dfd6ab14eb77cd2287f9
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-07-13 13:52:05 -0300

    Project creation

---
+++ github-consumer/app/src/main/java/br/com/argmax/githubconsumer/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.argmax.githubconsumer.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.argmax.githubconsumer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+
+}
